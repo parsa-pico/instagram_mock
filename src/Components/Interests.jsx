e73e14ebec5e06b0fbd1e03be41c83ef0092de31
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-04-01 18:55:24 +0430

    #

--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -3,19 +3,24 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import getInterestIcon from "../utils/interestsIcons";
 import useUser from "./../hooks/useUser";
 import interestsLevel from "./../utils/interestsLevel";
-import trash from "../Images/Icons/trash.png";
+import gear from "../Images/Icons/gear.svg";
 import plus from "../Images/Icons/plus.png";
 import floppy from "../Images/Icons/floppy.png";
 import { useNavigate } from "react-router-dom";
+import InterestsOffCanvas from "./InterestsOffCanvas";
+import { Button, Offcanvas } from "react-bootstrap";
 
 export default function Interests() {
   const [users, setUsers, currentUser, setCurrentUser] = useUser();
-
+  const inputRef = React.useRef(null);
   const [deleteCalled, setDeleteCalled] = useState(false);
-
+  const [offCanvasDetails, setOffCanvasDetails] = useState({});
   const [floppyClass, setFloppyClass] = useState(
     "img-fluid hidden floppy-icon"
   );
+
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+  const handleCloseOffCanvas = () => setShowOffCanvas(false);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -25,26 +30,33 @@ export default function Interests() {
   function deleteInterest(speciality) {
     const userIndex = localStorage.getItem("currentUserIndex");
     const copyUsers = [...users];
+
     const interestIndex = currentUser.interests.findIndex(
       (interest) => interest.speciality === speciality
     );
+    console.log(interestIndex);
     currentUser.interests.splice(interestIndex, 1);
     copyUsers[userIndex] = currentUser;
     setUsers(copyUsers);
     setDeleteCalled(true);
+    handleCloseOffCanvas();
   }
 
+  function handleShowOffCanvas(interest) {
+    setOffCanvasDetails(interest);
+    setShowOffCanvas(true);
+  }
   return (
     <div id="interests">
       <h1 className="interests__heading">interests</h1>
-      {
+      {/* {
         <div className="floppy-icon-wrapper">
           {deleteCalled && (
             <img className={"img-fluid cross-icon"} src={plus} />
           )}
           <img className={floppyClass} src={floppy} />
         </div>
-      }
+      } */}
       <div className="interests-wrapper">
         {Object.keys(currentUser) !== 0 &&
           currentUser.interests &&
@@ -52,7 +64,7 @@ export default function Interests() {
           currentUser.interests.map((interest, idx) => {
             return (
               <div
-                onClick={() => deleteInterest(interest.speciality)}
+                onClick={() => handleShowOffCanvas(interest)}
                 className="interest"
                 key={idx}
               >
@@ -61,7 +73,7 @@ export default function Interests() {
                   className="img-fluid interest__icon"
                   src={getInterestIcon(interest.speciality)}
                 />
-                <img className={"img-fluid interest__trash"} src={trash} />
+                <img className={"img-fluid interest__trash"} src={gear} />
               </div>
             );
           })}
@@ -73,6 +85,12 @@ export default function Interests() {
           <img className="img-fluid interest__icon interest__plus" src={plus} />
         </div>
       </div>
+      <InterestsOffCanvas
+        interest={offCanvasDetails}
+        show={showOffCanvas}
+        handleClose={handleCloseOffCanvas}
+        deleteInterest={deleteInterest}
+      />
     </div>
   );
 }
---
+++ src/Components/InterestsOffCanvas.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export default function InterestsOffCanvas({
+  show,
+  handleClose,
+  interest,
+  deleteInterest,
+}) {
+  return (
+    <Modal
+      backdropClassName="my-backdrop"
+      placement="top"
+      show={show}
+      onHide={handleClose}
+    >
+      <Modal.Header>
+        <Modal.Title className="interest-canvas-title">
+          {interest.speciality}
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body className="interest-canvas-body">
+        <h5>Category:</h5>
+        <h5 style={{ textTransform: "capitalize" }}>{interest.category}</h5>
+        <h5>Level:</h5>
+        <h5 style={{ textTransform: "capitalize" }}>{interest.level}</h5>
+        <Button
+          variant="danger"
+          className="interest-canvas-delete"
+          onClick={() => {
+            deleteInterest(interest.speciality);
+          }}
+        >
+          delete interest
+        </Button>
+      </Modal.Body>
+    </Modal>
+  );
+}
