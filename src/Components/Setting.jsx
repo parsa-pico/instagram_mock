689c0e5815c9e01942d54153d06c4b08ff37f907
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-04-04 14:01:37 +0330

    logout modal

--- src/Components/Setting.jsx
+++ src/Components/Setting.jsx
@@ -1,26 +1,25 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import useresContext from "../Context/useresContext";
 import Interests from "./Interests";
 import logoutIcon from "../Images/Icons/logout.png";
+import useUser from "./../hooks/useUser";
+
 export default function Setting() {
   const navigate = useNavigate();
-  const [currentUser, setCurrentUser] = useState({});
-  const [users, setUsers] = useContext(useresContext);
+  const [users, setUsers, currentUser, setCurrentUser] = useUser();
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
-  useEffect(() => {
-    const index = localStorage.getItem("currentUserIndex");
-    setCurrentUser(users[index]);
-  }, []);
   function handleLogout() {
     localStorage.clear();
     navigate("/login");
   }
+
   return (
     <div id="setting">
       <img
-        onClick={handleLogout}
+        onClick={() => setShowLogoutModal(true)}
         src={logoutIcon}
         className="img-fluid setting__logout"
       />
@@ -42,6 +41,29 @@ export default function Setting() {
         </Button>
       </div>
       <Interests />
+
+      <Modal
+        backdropClassName="my-backdrop"
+        placement="top"
+        show={showLogoutModal}
+        onHide={() => setShowLogoutModal(false)}
+      >
+        <Modal.Header>
+          <Modal.Title className="interest-canvas-title">Logout</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className="setting__modal__body">
+          <p>are you sure you want to logout?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowLogoutModal(false)}>
+            no,keep me logged in
+          </Button>
+          <Button variant="danger" onClick={handleLogout}>
+            yes,log me out!
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
